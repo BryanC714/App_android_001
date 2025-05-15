a99e52b620c6e755d1b6965aee448f2d6656a735
BryanC714 <165086565+BryanC714@users.noreply.github.com>
2025-05-14 20:03:40 -0500

    programacion

--- app/src/main/java/com/example/app_android_001/MainActivity.java
+++ app/src/main/java/com/example/app_android_001/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app_android_001;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Crear variables para la programacion
+    private EditText editTextUser;
+    private EditText editTextPassword;
+    private Button buttonOk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //obtener las referencias de los controles visuale(vistas) y asignarlas a las variables creadas
+        editTextUser = (EditText) findViewById(R.id.editTextUser);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonOk = findViewById(R.id.buttonOk);
+        /*buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickButtonOk(view);
+            }
+        });*/
+    }
+    public void clickButtonOk(View view) {
+        String user = editTextUser.getText().toString();
+        String password = editTextPassword.getText().toString();
+        if (!user.matches("") && !password.matches("")) {
+            //mostrar el actuvity principal
+        }else{
+            Toast.makeText(this,
+                    "Los datos son requeridos",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
